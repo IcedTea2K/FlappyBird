2c682ce5357da48c37e4890f91b4b0c1f91a4991
Minh Au <auduongminh2004@gmail.com>
2022-06-22 19:51:13 -0600

    added caption and icon

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import sys, pygame
-pygame.init()
+import sys
+import pygame as pg
+pg.init()
 
+# Setting up the Screen
 size = width, height = 320,240
 speed = [2,2]
-screen  = pygame.display.set_mode(size) # create scene
+screen  = pg.display.set_mode(size) # create scene
+pg.display.set_caption("Flappy Bird")
+icon = pg.image.load("flappy-bird-assets/favicon.png")
+pg.display.set_icon(icon)
 
+# Game Loop
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-    print(event)
-    screen.fill((0,0,0))
-    pygame.display.flip()
+    screen.fill((255,128,255))
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            sys.exit() # exit the program if the window is closed
+
+    pg.display.update()
