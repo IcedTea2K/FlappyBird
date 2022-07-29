bf2d7146f8e9079cf9db6b6862a55ccf95d457a3
Minh Au <auduongminh2004@gmail.com>
2022-07-29 10:47:22 +0700

    able to spawn upper and lower pipe with a definite amount of gap

--- Pipe.py
+++ Pipe.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import random
 class Pipe:
+    GAP = 100
     def __init__(self, screen, mode, speed) -> None:
         type = ["green", "red"]
         self.screen = screen
@@ -13,12 +14,16 @@ class Pipe:
             self.pipes = pg.image.load("flappy-bird-assets/sprites/pipe-" + "red" + ".png") 
         self.pos = [pg.display.get_surface().get_width(), pg.display.get_surface().get_height() - 112]
         w,h = self.pipes.get_size()
-        self.pipes = pg.transform.scale(self.pipes, (w, h*random.random())) # do not have to keep the original because it's only resized once
+        self.botPipe = pg.transform.scale(self.pipes, (w, h*random.random())) # do not have to keep the original because it's only resized once
+        w,h = self.botPipe.get_size()
+        self.topPipe = pg.transform.scale(self.pipes, (w, self.pos[1] - h - self.GAP))
+        self.topPipe = pg.transform.flip(self.topPipe, False, True)
 
     def move(self):
        self.pos[0] -= self.speed 
 
     def display(self):
         self.move()
-        self.screen.blit(self.pipes, self.pipes.get_rect(left=self.pos[0], bottom = self.pos[1]))
+        self.screen.blit(self.botPipe, self.botPipe.get_rect(left=self.pos[0], bottom = self.pos[1]))
+        self.screen.blit(self.topPipe, self.pipes.get_rect(left=self.pos[0], top = 0))
         print(self.pipes.get_size())  
